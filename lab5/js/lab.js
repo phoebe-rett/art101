d8275f084766002d1b8799455d271838390fa253
phoebe-rett <146970971+phoebe-rett@users.noreply.github.com>
2023-10-25 08:57:10 -0700

    lab 5; js, html, and css

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author: Phoebe Rettberg
+ * Created: 10.24.2023
+ * License: Public Domain
+ */
 
-// Constants
+//Define Variables
+var make= "Ford";
+var model= "Fusion";
+var color= "White";
+var year= 2009;
 
-// Functions
+//calculate
+age= 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("My car is a " + age + " year-old, " + year + " " + color + " " + make + " " + model + "<br>");
