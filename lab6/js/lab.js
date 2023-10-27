cc0eaa64b0ff6a2b715fd8dfea616125ec566f4c
phoebe-rett <146970971+phoebe-rett@users.noreply.github.com>
2023-10-27 08:22:09 -0700

    lab 6 changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author: Phoebe Rettberg
+ * Created: 10.26.2023
+ * License: Public Domain
+ */
 
-// Constants
 
-// Functions
+//Define Variables
+myTransport = ["my car", " my feet", " the loop bus", " the metro"];
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//Create an object for my main ride
+myMainRide= { 
+  make: "Ford", 
+  model: "Fusion", 
+  color: "White", 
+  year: 2009, 
+  age: function () {
+    return 2023-this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//output
+document.writeln("Kinds of transport I use: ", myTransport, "</br>");
 
-// let's get this party started
-main();
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>")
