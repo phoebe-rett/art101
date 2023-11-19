49e1ce9fc4c62f2b2add814b885c1d6bf4891217
phoebe-rett <146970971+phoebe-rett@users.noreply.github.com>
2023-11-19 13:12:29 -0800

    lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,6 +4,9 @@
  * License: Public Domain
  */
 
+//Credit to Wes Modes: 
+
+//New name, reverse alphabetical one word
 function sortString(inputString) {
     //split string into array, ignore spaces
     return inputString.split('').filter(e => e.trim().length)
@@ -13,8 +16,41 @@ function sortString(inputString) {
 
 // click listener for button
 $("#submit").click(function(){
+    //set value
     const userName = $("#user-name").val();
+    //apply sorting
     userNameSorted = sortString(userName);
+    //return sorted name
     $("#output p").append('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
 
+//New name, randomized with spaces, title case
+function sortStringBonus(inputString) {
+    //split
+    const nameArray = inputString.split('');
+    //randomize
+    for (let i = nameArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [nameArray[i], nameArray[j]] = [nameArray[j], nameArray[i]];
+    }
+    //join and return
+    const sortStringBonus = nameArray.join('');
+    return sortStringBonus;
+}
+ 
+//title case
+String.prototype.toTitleCase = function() {
+    return this.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
+
+// click listener for button
+$("#submit-two").click(function(){
+    // set value
+    const userNameBonus = $("#user-name-two").val();
+    // apply sort and title case
+    userNameSortedBonus = sortStringBonus(userNameBonus).toTitleCase();
+    // return new name
+    $("#output-two p").append('<div class="text"><p>' + userNameSortedBonus + '</p></div>');
+})
